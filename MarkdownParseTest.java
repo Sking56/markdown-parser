2e21dffd795a86bc8a694a1209bb3a71a42c0c1e
Sking56 <tingchen776@gmail.com>
2022-04-22 16:26:52 -0700

    javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest

---
+++ MarkdownParseTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+//Import the junit library
+import org.junit.*;
+//Create Test class
+public class MarkdownParseTest {
+    //Test declaration and create method to test addition
+    @Test
+    public void addition() {
+        //Self explanatory
+        assertEquals(2, 1 + 1);
+    }
+}
